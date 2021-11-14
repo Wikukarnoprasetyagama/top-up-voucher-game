19a92854b052bbdc5615ac95e1053e6753158d0e
Wikukarnoprasetyagama <prasetyagama2@gmail.com>
2021-11-15 00:05:09 +0700

    update file & add file & add images

---
+++ components/organisms/SideBar/MenuItem.tsx
@@ -0,0 +1,35 @@
+import cx from "classnames";
+import Image from "next/image";
+interface MenuItemProps {
+  title: string;
+  active?: boolean;
+  icon:
+    | "ic-menu-overview"
+    | "ic-menu-transaction"
+    | "ic-menu-message"
+    | "ic-menu-card"
+    | "ic-menu-rewards"
+    | "ic-menu-setting"
+    | "ic-menu-logout";
+}
+
+export default function MenuItem(props: Partial<MenuItemProps>) {
+  const { title, icon, active } = props;
+  const classItem = cx({
+    item: true,
+    "mb-30": true,
+    active,
+  });
+  return (
+    <div className={classItem}>
+      <div className="me-3">
+        <Image src={`/icon/${icon}.svg`} width={25} height={25} />
+      </div>
+      <p className="item-title m-0">
+        <a href="" className="text-lg text-decoration-none">
+          {title}
+        </a>
+      </p>
+    </div>
+  );
+}
